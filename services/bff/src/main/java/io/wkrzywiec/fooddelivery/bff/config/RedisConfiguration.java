c2759ea0986aa330a5127d6906778bae58de1200
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2023-07-16 16:01:27 +0200

    extract InboxMessageProcessor

--- services/bff/src/main/java/io/wkrzywiec/fooddelivery/bff/config/RedisConfiguration.java
+++ services/bff/src/main/java/io/wkrzywiec/fooddelivery/bff/config/RedisConfiguration.java
@@ -4,17 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.sonus21.rqueue.core.RqueueEndpointManager;
 import com.github.sonus21.rqueue.core.RqueueMessageEnqueuer;
 import com.redislabs.lettusearch.StatefulRediSearchConnection;
-import io.wkrzywiec.fooddelivery.bff.inbox.InboxPublisher;
-import io.wkrzywiec.fooddelivery.bff.inbox.RedisInboxListener;
-import io.wkrzywiec.fooddelivery.bff.inbox.RedisInboxPublisher;
-import io.wkrzywiec.fooddelivery.bff.inbox.RedisQueueCreator;
+import io.wkrzywiec.fooddelivery.bff.inbox.*;
 import io.wkrzywiec.fooddelivery.bff.repository.DeliveryViewRepository;
 import io.wkrzywiec.fooddelivery.bff.repository.RedisDeliveryViewRepository;
 import io.wkrzywiec.fooddelivery.bff.repository.RedisFoodItemRepository;
-import io.wkrzywiec.fooddelivery.bff.view.DeliveryStatus;
 import io.wkrzywiec.fooddelivery.bff.view.RedisDeliveryViewProcessor;
 import io.wkrzywiec.fooddelivery.bff.view.RedisOrdersChannelConsumer;
-import io.wkrzywiec.fooddelivery.commons.infra.messaging.MessagePublisher;
 import io.wkrzywiec.fooddelivery.commons.infra.messaging.redis.RedisMessageConsumerConfig;
 import io.wkrzywiec.fooddelivery.commons.infra.messaging.redis.RedisStreamListener;
 import lombok.extern.slf4j.Slf4j;
@@ -24,8 +19,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.stream.Subscription;
-
-import java.time.Clock;
 
 @Slf4j
 @Configuration
@@ -50,8 +43,8 @@ public class RedisConfiguration extends RedisMessageConsumerConfig {
     }
 
     @Bean
-    public RedisInboxListener redisInboxListener(MessagePublisher messagePublisher, Clock clock) {
-        return new RedisInboxListener(messagePublisher, clock);
+    public RedisInboxListener redisInboxListener(InboxMessageProcessor inboxMessageProcessor) {
+        return new RedisInboxListener(inboxMessageProcessor);
     }
 
     @Bean
