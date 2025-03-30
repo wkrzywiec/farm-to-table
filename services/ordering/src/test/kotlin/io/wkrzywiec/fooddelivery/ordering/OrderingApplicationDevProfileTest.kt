e5564b6ee16ad46570d9ffe422d0e07ac01daaa1
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2025-03-30 18:25:00 +0200

    @DirtiesContext

--- services/ordering/src/test/kotlin/io/wkrzywiec/fooddelivery/ordering/OrderingApplicationDevProfileTest.kt
+++ services/ordering/src/test/kotlin/io/wkrzywiec/fooddelivery/ordering/OrderingApplicationDevProfileTest.kt
@@ -1,8 +1,10 @@
 package io.wkrzywiec.fooddelivery.ordering
 
 import org.junit.jupiter.api.Test
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
+@DirtiesContext
 @ActiveProfiles("dev")
 class OrderingApplicationDevProfileTest: IntegrationTest() {
 
